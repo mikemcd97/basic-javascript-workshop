b4a5e46b23098907b72d0319c5c2bf1958074ba1
Mike McDonald <mikemcd97@gmail.com>
2017-07-03 19:08:14 +0000

    reverseString

--- workshop.js
+++ workshop.js
@@ -77,7 +77,7 @@ function repeatString(inputString, numRepetitions) {
 }
 
 function reverseString(inputString) {
-
+    return inputString.split('').reverse().join('');
 }
 
 function longestWord(inputString) {

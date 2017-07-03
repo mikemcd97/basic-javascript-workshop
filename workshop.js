38798d1bd4a01ac58805a78b98d3d99cd2872e01
Mike McDonald <mikemcd97@gmail.com>
2017-07-03 18:38:11 +0000

    implemented multiplyTwoNumbers function

--- workshop.js
+++ workshop.js
@@ -34,9 +34,13 @@ function addTwoNumbers(num1, num2) {
 }
 
 function multiplyTwoNumbers(num1, num2) {
-
+    if(typeof num1 === 'number' && typeof num2 === 'number'){
+        return(num1 * num2);
+    }
+    else{
+        return undefined;
+    }
 }
-
 function calculator(operation, num1, num2) {
 
 }
